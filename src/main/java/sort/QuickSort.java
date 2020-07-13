9327027b8d2e5abbdd9e753037ca17c6556c6292
gaopan <fenggaopan1991@qq.com>
2020-07-13 22:45:50 +0800

    增加快速排序

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -24,28 +24,25 @@ public class QuickSort {
      * @param arr
      */
     public static void quickSort(int[] arr, int left, int right) {
-        int l = left;
-        int r = right;
-
-        //pivot是中轴
-        int pivot = arr[(left + right) /2];
-        //while循环的目的是让比pivot值小的放到左边，大的放到右边
-        int temp = 0; //交换临时变量
-        while(l<r) {
-
-
-            //左边找一个大于等于pivot
-            while(arr[l] < pivot) {
-                l+=1;
+        int l = left; //左下标
+        int r = right; //右下标
+        //pivot 中轴值
+        int pivot = arr[(left + right) / 2];
+        int temp = 0; //临时变量，作为交换时使用
+        //while循环的目的是让比pivot 值小放到左边
+        //比pivot 值大放到右边
+        while( l < r) {
+            //在pivot的左边一直找,找到大于等于pivot值,才退出
+            while( arr[l] < pivot) {
+                l += 1;
             }
-
-            //pivot右边找小于等于
+            //在pivot的右边一直找,找到小于等于pivot值,才退出
             while(arr[r] > pivot) {
-                r-=1;
+                r -= 1;
             }
-
-            //说明左边全部是小于pivot了
-            while(l >= r) {
+            //如果l >= r说明pivot 的左右两的值，已经按照左边全部是
+            //小于等于pivot值，右边全部是大于等于pivot值
+            if( l >= r) {
                 break;
             }
 
@@ -54,28 +51,25 @@ public class QuickSort {
             arr[l] = arr[r];
             arr[r] = temp;
 
-            //交换后，发现arr[l] ==  pivot值
-            //前移
+            //如果交换完后，发现这个arr[l] == pivot值 相等 r--， 前移
             if(arr[l] == pivot) {
-                r -=1;
+                r -= 1;
             }
-
+            //如果交换完后，发现这个arr[r] == pivot值 相等 l++， 后移
             if(arr[r] == pivot) {
-                l +=1;
+                l += 1;
             }
         }
 
-        //如果l==r 必须l++, r-- 要不然死循环
-        if(l == r) {
-            l +=1;
-            r -=1;
+        // 如果 l == r, 必须l++, r--, 否则为出现栈溢出
+        if (l == r) {
+            l += 1;
+            r -= 1;
         }
-
         //向左递归
         if(left < r) {
             quickSort(arr, left, r);
         }
-
         //向右递归
         if(right > l) {
             quickSort(arr, l, right);
